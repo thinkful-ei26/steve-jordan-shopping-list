267b84fa41967bf61e8111f7d4f4e54cbc7e8466
Stephen Anderson <anderssj@usc.edu>
2018-11-14 09:14:39 -0700

    first

--- server.js
+++ server.js
@@ -1,23 +1,21 @@
-
 const express = require('express');
 // we'll use morgan to log the HTTP layer
 const morgan = require('morgan');
-// we'll use body-parser's json() method to 
+// we'll use body-parser's json() method to
 // parse JSON data sent in requests to this app
 const bodyParser = require('body-parser');
 
 // we import the ShoppingList model, which we'll
 // interact with in our GET endpoint
-const {ShoppingList} = require('./models');
+const { ShoppingList } = require('./models');
 
 const jsonParser = bodyParser.json();
 const app = express();
 
-// log the http layer
 app.use(morgan('common'));
 
 // we're going to add some items to ShoppingList
-// so there's some data to look at. Note that 
+// so there's some data to look at. Note that
 // normally you wouldn't do this. Usually your
 // server will simply expose the state of the
 // underlying database.
