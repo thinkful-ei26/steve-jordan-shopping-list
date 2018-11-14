1cdfc4599b4bf6496dbfb444e603eeafee760e8a
Stephen Anderson <anderssj@usc.edu>
2018-11-14 09:33:19 -0700

    delete recipes

--- server.js
+++ server.js
@@ -66,6 +66,18 @@ app.post('/recipes', jsonParser, (req, res) => {
   res.status(201).json(item);
 });
 
+app.delete('/shopping-list/:id', (req, res) => {
+  ShoppingList.delete(req.params.id);
+  console.log(`Deleted shopping list item \`${req.params.id}\``);
+  res.status(204).end();
+});
+
+app.delete('/recipes/:id', (req, res) => {
+  Recipes.delete(req.params.id);
+  console.log(`Deleted Recipe list item \`${req.params.id}\``);
+  res.status(204).end();
+});
+
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
 });
